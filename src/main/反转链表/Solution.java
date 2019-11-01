3941689b93725ebcef2990aa6cf44032ecfa42dd
chenyang <shuiyue@bxjjren.com>
2019-11-01 17:57:05 +0800

    还差一个递归解法

--- src/main/反转链表/Solution.java
+++ src/main/反转链表/Solution.java
@@ -6,6 +6,28 @@ import java.util.LinkedList;
 
 public class Solution {
 
+    /**
+     * 指针向前指法
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode tempNext = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = tempNext;
+        }
+        return pre;
+    }
+
+    /**
+     * 用外部栈处理的解法 内存超出限制
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         LinkedList<ListNode> list = new LinkedList<>();
         list.addFirst(null);
